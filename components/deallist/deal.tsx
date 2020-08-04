d1e1893c21751a70730a85250b6ee4748977f0ff
James Nicholson <james.n@tencent.co.za>
2020-08-04 21:22:07 +0200

    Header Component added, Feed Componenet cleaned up, Deal Component refactored

--- components/deallist/deal.tsx
+++ components/deallist/deal.tsx
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect} from 'react';
 import { Image, StyleSheet } from 'react-native';
-import {  Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import { Card, CardItem, Thumbnail, Text, Button, Left, Body, Right } from 'native-base';
+import Icon from 'react-native-vector-icons/Ionicons'
 import IDeal from "../../api/interfaces"
 
 interface IProps{
@@ -10,18 +11,29 @@ interface IProps{
 const DealCard : React.FC<IProps> = props => {
     console.log(props.deal.title)
     return <Card>
-            <CardItem>
-                <Left>
-                    <Thumbnail source={{uri: props.deal.image225}} />
-                    <Body>
-                    <Text>{props.deal.title}</Text>
-                    <Text note>GeekyAnts</Text>
-                    </Body>
-                </Left>
-            </CardItem>
-            <CardItem cardBody>
-                <Image source={{uri: props.deal.image225}} style={styles.img}/>
-            </CardItem>
+                <CardItem>
+                    <Left>
+                        <Thumbnail source={{uri: props.deal.image225}} />
+                        <Body>
+                            <Text>{props.deal.title}</Text>
+                            <Text note>GeekyAnts</Text>
+                        </Body>
+                    </Left>
+        
+                </CardItem>
+                <CardItem cardBody>
+                    <Image source={{uri: props.deal.image225}} style={styles.img}/>
+                </CardItem>
+                <CardItem>
+                    <Left>
+                        <Button rounded danger>
+                            <Text>Buy Now</Text>
+                        </Button>
+                    </Left>
+                    <Right>
+                        <Icon style={styles.favorties} name="heart" />
+                    </Right>
+                </CardItem>
         </Card>
 }
 const styles = StyleSheet.create({
@@ -29,6 +41,9 @@ const styles = StyleSheet.create({
         height: 200, 
         width: '100%', 
         flex: 1
+    },
+    favorties:{
+        fontSize:20
     }
   });
 export default DealCard
